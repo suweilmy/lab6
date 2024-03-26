c3693b5f0b64020f1a7624e41dbb438df773755f
suweilmy <saud.a.suw@gmail.com>
2024-03-26 06:50:48 +0300

    .

--- lab777/app1/models.py
+++ lab777/app1/models.py
@@ -4,6 +4,7 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     email = models.EmailField()
+    nationality = models.CharField(max_length=100)
     courses = models.ManyToManyField('Course', related_name='students')
 
 class Course(models.Model):
